0e3ce18705df5ebc3a1c68f0531c42c39178d206
Administrator <lingdirk2009@163.com>
2016-03-17 12:08:19 +0800

    Add TextSwitcher, ViewStub and Custom View test.

---
+++ app/src/main/java/com/curry/stephen/contentprovidertest/CascadeLayout.java
@@ -0,0 +1,20 @@
+package com.curry.stephen.contentprovidertest;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * Created by LingChong on 2016/3/17 0017.
+ */
+public class CascadeLayout extends ViewGroup {
+    
+    public CascadeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
